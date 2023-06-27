8dfaaf8b4b495fff465c23cf777b8132e3db893b
EjioforSandraAkunna <sandra.ejiofor@gmail.com>
2023-06-27 15:50:54 +0200

    sandra version

---
+++ routes/new.routes.js
@@ -0,0 +1,9 @@
+// const express = require("express");
+// const router = express.Router();
+
+// /* GET home page */
+// router.get("/newroutes", (req, res, next) => {
+//   res.send("this my new route");
+// });
+
+// module.exports = router;
